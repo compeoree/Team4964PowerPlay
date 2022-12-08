0ce284379d43670540a17f41575ac0286f16c8d4
Louis <compeoree@users.noreply.github.com>
2022-12-07 21:14:31 -0600

    Initial working Autonomous Left

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Field/ParkingAutoLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Field/ParkingAutoLeft.java
@@ -14,10 +14,11 @@ public class ParkingAutoLeft extends LinearOpMode {
 
     Bot robot = new Bot();
     Variables var = new Variables();
+    ObjectDetector.POSITIONS pos;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        ObjectDetector detector = new ObjectDetector(this, true, false);
+        ObjectDetector detector = new ObjectDetector(this, true,false);
 
         robot.init(hardwareMap, this);
 
@@ -25,49 +26,67 @@ public class ParkingAutoLeft extends LinearOpMode {
         Bot.tLeftDT.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         Bot.bRightDT.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         Bot.tRightDT.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Bot.Lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Bot.Claw.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         waitForStart();
+
+        // camera decision
         ObjectDetector.POSITIONS position = detector.getDecision(this);
+        pos = position;
         telemetry.addData("position ", detector.getDecision(this));
 
+        ACTI();
 
-        //robot.strafeDrive(-40, 0, 0.7, this);
-        //robot.strafeDrive(0,70, 0.7, this);
-    //   // robot.strafeDrive(-39, 0, 0.7, this);
-    //   Bot.strafeDrive(-40,.7, this);
-    //   Bot.driveStraight(.7, 70, 70, 70, 70,  this);
-    //   Bot.strafeDrive(39,.7, this);
+        ACTII();
 
+        ACTIII();
+    }
 
-    //   // getting into position to drop cone
-    //   Bot.Lift.setTargetPosition(var.Lvl_Tall);
-    //   //robot.strafeDrive(0, 4, 0.7, this);
-    //   Bot.driveStr//aight(.7, 4, 4, 4, 4,  this);
+    void ACTI(){
+        Bot.Claw.setTargetPosition(var.claw_cone);
+        sleep(1);
+        Bot.Claw.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Bot.Claw.setPower(1);
+        sleep(550);
+        Bot.strafeDrive(84,.5, this);
+        sleep(5);
+        Bot.driveStraight(125,.5, this);
+        sleep(5);
+        Bot.strafeDrive(-33,.3,this);
+    }
 
-    //   Bot.Claw.setTargetPosition(var.claw_open);
-    //   //robot.strafeDrive(0, -4, 0.7, this);
-    //   Bot.driveStraight(.7, -4, -4, -4, -4, this);
-    //   Bot.Lift.setTargetPosition(var.Lvl_Ground);
-    //   Bot.Claw.setTargetPosition(var.claw_zero);
-    //   //robot.strafeDrive(30, 0, 0.7, this);
-    //   //robot.strafeDrive(0, 65, 0.7, this);
-    //   Bot.strafeDrive(-30,.7, this);
-    //   Bot.driveStraight(.7, 65, 65, 65, 65, this);
+    void ACTII(){
+        Bot.Lift.setTargetPosition(var.Lvl_Tall);
+        sleep(1);
+        Bot.Lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Bot.Lift.setPower(1);
+        sleep(-var.Lvl_Tall);
+        Bot.driveStraight(16, .3, this);
+        Bot.Lift.setTargetPosition(var.Lvl_Tall + 150);
+        sleep(75);
+        //Bot.strafeDrive(3,.5,this);
+        Bot.Claw.setTargetPosition(var.claw_zero);
+        sleep(2000);
+        Bot.driveStraight(-20, .3, this);
+        sleep(1);
+        Bot.Claw.setTargetPosition(var.claw_cone);
+        sleep(1200);
+        Bot.Lift.setTargetPosition(var.Lvl_Ground);
+        //sleep(-var.Lvl_Tall);
+        Bot.Lift.setPower(0);
+    }
 
-
-        // make the decision
-        switch (position) {
+    void ACTIII(){
+        switch (pos) {
             case POS1:
+                Bot.strafeDrive(-95,.7,this);
                 break;
             case POS2:
-                //robot.strafeDrive(55, 0, 0.7, this);
-                Bot.strafeDrive(-55,.7, this);
-
+                Bot.strafeDrive(-30,.7,this);
                 break;
             case POS3:
-                //robot.strafeDrive(112, 0, 0.7, this);
-                Bot.strafeDrive(-112,.7, this);
-
+                Bot.strafeDrive(35,.7,this);
         }
     }
 }
